61925a29e083b43b7c2ec2f88d7f778f91a1a04e
Greenboi123 <pr21872@bristol.ac.uk>
2022-02-26 17:12:37 +0000

    just added comments to understand better

--- ControlCode.py
+++ ControlCode.py
@@ -19,56 +19,53 @@ def timetocrossacceleration(meters, cars):
     return 3*cars + (acceleration/2.591)**(1/2) - 1.5
 
 def conjestion(x,y):
+    #x is cars on left #y is cars on right
     distances = [5,20,60]
     rounds = 3
-    left = x
-    right = y
     leftx = []
     righty = []
     timeright = []
     timeleft = []
     leftfull = []
     rightfull = []
-    while left>0:
-        going = random.randint(1,x)
-        leftx.append(going)
-        left -= going
-        x -= going
-    while right >0:
-        going = random.randint(1,y)
+    while x>0:
+        going = random.randint(1,x) #creates a random interger in range 1 to x (including x)
+        leftx.append(going) #places that random interger into leftx list
+        x -= going #takes the cars that have gone away from cars on the left
+    while y>0:
+        going = random.randint(1,y) 
         righty.append(going)
-        right -= going
         y -= going
-    if len(leftx) > len(righty):
-        for i in range(0,(len(righty))):
+    if len(leftx) > len(righty): #this is because they both take different combinations to reach 0 
+        for i in range(0,(len(righty))): #right is less than left so this takes parts from left that are equal to the number parts in right and places them into time right
             timeright.append(leftx[i])
             timeright.append(righty[i])
-            timeleft.append(righty[i])
+            timeleft.append(righty[i]) #basically places all of right into time left 
         for i in range(0,len(leftx)):
-            timeleft.append(leftx[i])
-    elif len(righty) > len(leftx):
-        for i in range(0,(len(leftx))):
+            timeleft.append(leftx[i]) #places all of left into time left
+    elif len(righty) > len(leftx): #same but switched
+        for i in range(0,(len(leftx))): 
             timeleft.append(leftx[i])
             timeleft.append(righty[i])
             timeright.append(leftx[i])
         for i in range(0,len(righty)):
             timeright.append(righty[i])
-    elif len(righty) == len(leftx):
+    elif len(righty) == len(leftx): # if they are equal just place both leftx and righty into both time left and time right
         for i in range(0,(len(leftx))):
             timeleft.append(leftx[i])
             timeleft.append(righty[i])
             timeright.append(leftx[i])
             timeleft.append(righty[i])
-    for j in distances:
-        for i in timeleft:
-            leftfull.append(timetocrossconstant(j, i))
-            leftfull.append(timetocrossacceleration(j, i))
+    for j in distances: #cycles through all the distances
+        for i in timeleft: 
+            leftfull.append(timetocrossconstant(j, i))  #calculates the time of each group of cars under distance with speed = 10mph
+            leftfull.append(timetocrossacceleration(j, i)) #calculates the time of each group of cars under distance with speed = 10mph
         for z in timeright:
             rightfull.append(timetocrossconstant(j, z))
             rightfull.append(timetocrossacceleration(j, z))
-    return abs(round(sum(rightfull)/(6)-sum(leftfull)/(6),rounds))
+    return abs(round(sum(rightfull)/(6)-sum(leftfull)/(6),rounds)) #only 6 different sceanrios therefore average is totaltime/6. abs as right - left could be negetive but just want difference.
 
-def accuratetime(x,y):
+def accuratetime(x,y): # adds up conjestion(x,y) n times and then divides by n to get more accurate average
     n = 20000
     rounds = 3
     accuratetime = 0
